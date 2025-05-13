c82c94e7ef02b7da48aaa86f0fd7007787479dd8
Shreya <shreyama06@gmail.com>
2025-05-13 14:48:33 +0530

    first commit

---
+++ repo_name/src/main/java/com/shreya123-456/App.java
@@ -0,0 +1,13 @@
+package com.shreya123-456;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
